d6162d2e770e17144e17a217f664d18e74da4b8e
uzzsen <uzzsen@gmail.com>
2015-11-11 20:51:06 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,48 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+ def guess(letter)
+    
+    raise ArgumentError if letter.nil? || letter=~/[^\w]/ || letter.empty?
+    
+    return @guesses << letter if @word.include?(letter) && @guesses.empty?
+    
+    if @word.downcase.include?(letter.downcase) && @guesses.downcase.include?(letter.downcase) then
+      return false
+    elsif @word.include?(letter) && !@guesses.include?(letter) then
+      @guesses << letter  
+    elsif !@wrong_guesses.downcase.include?(letter.downcase) then
+      @wrong_guesses << letter
+    else
+      false
+    end  
+    
+ end
+  
+ def word_with_guesses
+   @word.chars.map { |wl|
+   @guesses.chars.any? { |gl| wl == gl} ? wl : "-"
+    }.join
+ end
 
+ def check_win_or_lose
+   if word_with_guesses == @word
+     :win
+   elsif @wrong_guesses.length >=7
+     :lose
+   elsif word_with_guesses != @word && @wrong_guesses.length <7
+     :play
+   end   
+ end  
+ 
   def self.get_random_word
     require 'uri'
     require 'net/http'
