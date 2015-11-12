64cd845fc77d89e336552637216ad8e946b1aa22
uzzsen <uzzsen@gmail.com>
2015-11-12 22:48:38 +0000

    4 features completed

--- app.rb
+++ app.rb
@@ -40,6 +40,7 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    flash[:message] ="You have already used that letter" unless @game.guess letter
     redirect '/show'
   end
   
@@ -50,6 +51,9 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    redirect '/win' if @game.check_win_or_lose == :win
+    redirect '/lose' if @game.check_win_or_lose == :lose
+    
     erb :show # You may change/remove this line
   end
   
